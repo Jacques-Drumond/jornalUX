cfdae7b286f9e95c45c82e10ebece3bd8bc67729
Jacques Drumond <jacques.drumond@matrix.local>
2023-05-22 15:56:39 -0300

    second

--- jornal_page/views.py
+++ jornal_page/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, FileResponse
 # Create your views here.
 
 def index(request):
     try:
-        return HttpResponse("Funcionando")
+        pdf_file = open(r"C:\Users\jacques.drumond\Desktop\jornal\_New Ux Times Abril (1).pdf", "rb")
+        return FileResponse(pdf_file, content_type='application/pdf')
     except:
         return HttpResponseNotFound("Não funcionou")
